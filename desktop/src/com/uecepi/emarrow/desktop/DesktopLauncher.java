ec473a5f3e410eb357a348e6264ad4bded2f939b
Clement PY <clemicle@hotmail.fr>
2022-02-09 11:25:40 +0100

    Initial commit

---
+++ desktop/src/com/uecepi/emarrow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uecepi.emarrow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uecepi.emarrow.Emarrow;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Emarrow(), config);
+	}
+}
