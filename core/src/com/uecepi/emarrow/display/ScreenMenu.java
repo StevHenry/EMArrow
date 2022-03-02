8edd3a6a2f7caf0cfcdfb01436360c44535a3b38
Jean-Baptiste <jean-baptiste.trognon@mines-ales.org>
2022-03-02 10:35:39 +0100

    Ou peut être dans l'armoire

---
+++ core/src/com/uecepi/emarrow/display/ScreenMenu.java
@@ -0,0 +1,60 @@
+package com.uecepi.emarrow.display;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class ScreenMenu implements Screen {
+
+    protected Stage stage;
+    protected Table table;
+
+    public ScreenMenu() {
+        stage = new Stage();
+
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+
+    //____________________________________________//
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
