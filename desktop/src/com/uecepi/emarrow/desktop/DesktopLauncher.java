1887e36be3eca006400b70b37e887ecfe43f8aa6
Jean-Baptiste <jean-baptiste.trognon@mines-ales.org>
2022-03-02 09:50:59 +0100

    Ou peut être dans l'armoire

--- desktop/src/com/uecepi/emarrow/desktop/DesktopLauncher.java
+++ desktop/src/com/uecepi/emarrow/desktop/DesktopLauncher.java
@@ -1,12 +1,26 @@
 package com.uecepi.emarrow.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.uecepi.emarrow.Emarrow;
+import com.uecepi.emarrow.map.Map;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Emarrow(), config);
+		configure(config);
+		new LwjglApplication(new Map(), config);
 	}
+
+	private static void configure(LwjglApplicationConfiguration config) {
+		config.title = "Em'Arrow";
+		config.width = 1740;
+		config.height = 950;
+		config.addIcon("images/icon.png", Files.FileType.Internal);
+		config.resizable = false;
+		config.x = 80;
+		config.y = 0;
+	}
+
 }
