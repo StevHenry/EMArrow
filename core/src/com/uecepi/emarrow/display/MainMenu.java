811c95b628ad40ff39025f7f3286fd4df0e5790b
Ariane Bouthors <ari.bouthors1@gmail.com>
2022-03-09 12:00:26 +0100

    ExitButton

--- core/src/com/uecepi/emarrow/display/MainMenu.java
+++ core/src/com/uecepi/emarrow/display/MainMenu.java
@@ -1,5 +1,6 @@
 package com.uecepi.emarrow.display;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -26,21 +27,27 @@ public class MainMenu extends ScreenMenu{
 
     private void create() {
         emarrow = new Label("Em'Arrow", skin);
-        table.add(emarrow);
-        table.row();
+        table.add(emarrow).row();
+        emarrow.setFontScale(10);
 
         playButton = new TextButton("Play", skin);
-        table.add(playButton);
-        table.row();
+        table.add(playButton).height(100).width(200).padTop(30).row();
         onClick(playButton);
+        //playButton.setLabel().setFontScale(10);
 
         controlButton = new TextButton("Controls", skin);
-        table.add(controlButton);
-        table.row();
+        table.add(controlButton).height(100).width(200).padTop(30).row();
 
         exitButton = new TextButton("Exit", skin);
-        table.add(exitButton);
-        table.row();
+        table.add(exitButton).height(100).width(200).padTop(30).row();
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+
     }
 
     private void onClick(TextButton button){
