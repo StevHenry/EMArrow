41a8319019a840faa604fba5bbd5f0ba1ba7ca0d
Jean-Baptiste <jean-baptiste.trognon@mines-ales.org>
2022-03-09 10:42:50 +0100

    tileset

--- core/assets/images/Emarrow.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="tileset" tilewidth="8" tileheight="8" tilecount="40" columns="5">
- <image source="tileset.png" width="40" height="64"/>
-</tileset>
