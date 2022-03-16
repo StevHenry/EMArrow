86b3d5a16fc8e3366bd5323f66a1071c86b41113
Jean-Baptiste <jean-baptiste.trognon@mines-ales.org>
2022-03-16 11:15:29 +0100

    add Animator class, and loads from Assets

--- core/src/com/uecepi/emarrow/assets/Assets.java
+++ core/src/com/uecepi/emarrow/assets/Assets.java
@@ -3,22 +3,33 @@ package com.uecepi.emarrow.assets;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.SkinLoader;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.uecepi.emarrow.display.Animator;
+
+import java.util.HashMap;
 
 public class Assets {
+
 
     private static AssetManager assetManager = new AssetManager();
 
     public static final AssetDescriptor<Skin> SKIN_VIS = new AssetDescriptor<>("default/uiskin.json", Skin.class, new SkinLoader.SkinParameter("default/uiskin.atlas"));
 
+    // Animation sprite sheets
+    public static final HashMap<String, Animation<TextureRegion>> ANIMATIONS = new HashMap<>();
+
     public static void load() {
+        Animator.load();
         assetManager.load(SKIN_VIS);
         assetManager.finishLoading();
+
     }
 
     public static AssetManager getAssetManager() {
         return assetManager;
     }
-
 
 }
