4d984ae486b8175300d942b2e780b317ec45219e
Jean-Baptiste <jean-baptiste.trognon@mines-ales.org>
2022-04-13 20:22:37 +0200

    new tiles

---
+++ core/assets/maps/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset2" tilewidth="16" tileheight="16" tilecount="24" columns="4">
+ <image source="tileset2.png" width="64" height="96"/>
+</tileset>
